faf40ce819dd2564bd2f0316fa808701821abab1
Ximena Lope Guevara <99635154+XimenaLope@users.noreply.github.com>
2023-04-01 23:07:27 -0600

    Create foto-lista.js

---
+++ foto-lista.js
@@ -0,0 +1,11 @@
+const PHOTO_LIST =[
+    'images/conejo.jpg',
+    'images/lobo.jpg',
+    'images/gato.jpg',
+    'images/mono.jpg',
+    'images/panda.jpg',
+    'images/perro.jpg',
+    'images/suricata.jpg',
+    'images/tigre.jpg',
+    'images/zorro.jpg',
+];
